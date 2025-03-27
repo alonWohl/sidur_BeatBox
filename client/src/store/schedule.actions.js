f9f69a8fec30dae720442026cece9c0d8a43d51a
oren wohl <orenwohlwohl@gmail.com>
2025-03-27 18:13:23 +0200

    Add schedule actions and reducer for managing schedules in the store. Implement functions for loading, adding, updating, and removing schedules, along with handling messages. Establish initial state and action types for schedule management.

---
+++ client/src/store/schedule.actions.js
@@ -0,0 +1,127 @@
+import {scheduleService} from '../services/schedule/schedule.service.remote';
+import {store} from './store';
+import {
+	ADD_SCHEDULE,
+	REMOVE_SCHEDULE,
+	SET_SCHEDULES,
+	SET_SCHEDULE,
+	UPDATE_SCHEDULE,
+	ADD_SCHEDULE_MSG,
+} from './schedule.reducer';
+
+export async function loadSchedules(filterBy) {
+	try {
+		const schedules = await scheduleService.query({...filterBy});
+		store.dispatch(getCmdSetSchedules(schedules));
+	} catch (err) {
+		console.log('Cannot load schedules', err);
+		throw err;
+	}
+}
+
+export async function loadSchedule(branchName) {
+	try {
+		const schedule = await scheduleService.getScheduleByBranchName(branchName);
+		store.dispatch(getCmdSetSchedule(schedule));
+	} catch (err) {
+		console.log('Cannot load schedule', err);
+		throw err;
+	}
+}
+
+export async function removeSchedule(scheduleId) {
+	try {
+		await scheduleService.remove(scheduleId);
+		store.dispatch(getCmdRemoveSchedule(scheduleId));
+	} catch (err) {
+		console.log('Cannot remove schedule', err);
+		throw err;
+	}
+}
+
+export async function addSchedule(schedule) {
+	try {
+		if (schedule.name.length < 2) {
+			throw new Error('שם העובד חייב להכיל לפחות 2 תווים');
+		}
+
+		const savedSchedule = await scheduleService.save(schedule);
+		store.dispatch(getCmdAddSchedule(savedSchedule));
+		return savedSchedule;
+	} catch (err) {
+		console.log('Cannot add schedule', err);
+		throw err;
+	}
+}
+
+export async function updateSchedule(schedule) {
+	try {
+		const savedSchedule = await scheduleService.save(schedule);
+		store.dispatch(getCmdUpdateSchedule(savedSchedule));
+		return savedSchedule;
+	} catch (err) {
+		console.log('Cannot save schedule', err);
+		throw err;
+	}
+}
+
+export async function addWorkerMsg(workerId, txt) {
+	try {
+		const msg = await scheduleService.addScheduleMsg(scheduleId, txt);
+		store.dispatch(getCmdAddScheduleMsg(msg));
+		return msg;
+	} catch (err) {
+		console.log('Cannot add schedule msg', err);
+		throw err;
+	}
+}
+
+// Command Creators:
+function getCmdSetSchedules(schedules) {
+	return {
+		type: SET_SCHEDULES,
+		schedules,
+	};
+}
+function getCmdSetSchedule(schedule) {
+	return {
+		type: SET_SCHEDULE,
+		schedule,
+	};
+}
+function getCmdRemoveSchedule(scheduleId) {
+	return {
+		type: REMOVE_WORKER,
+		workerId,
+	};
+}
+function getCmdAddSchedule(schedule) {
+	return {
+		type: ADD_SCHEDULE,
+		schedule,
+	};
+}
+function getCmdUpdateSchedule(schedule) {
+	return {
+		type: UPDATE_SCHEDULE,
+		schedule,
+	};
+}
+function getCmdAddScheduleMsg(msg) {
+	return {
+		type: ADD_SCHEDULE_MSG,
+		msg,
+	};
+}
+
+// // unitTestActions()
+// async function unitTestActions() {
+// 	await loadWorkers();
+// 	await addWorker(workerService.getEmptyWorker());
+// 	await updateWorker({
+// 		_id: 'm1oC7',
+// 		title: 'Worker-Good',
+// 	});
+// 	await removeWorker('m1oC7');
+// 	// TODO unit test addWorkerMsg
+// }
---
+++ client/src/store/schedule.reducer.js
@@ -0,0 +1,50 @@
+export const SET_SCHEDULES = 'SET_SCHEDULES';
+export const SET_SCHEDULE = 'SET_SCHEDULE';
+export const REMOVE_SCHEDULE = 'REMOVE_SCHEDULE';
+export const ADD_SCHEDULE = 'ADD_SCHEDULE';
+export const UPDATE_SCHEDULE = 'UPDATE_SCHEDULE';
+export const ADD_SCHEDULE_MSG = 'ADD_SCHEDULE_MSG';
+export const SET_ERROR = 'SET_ERROR';
+
+const initialState = {
+	schedules: [],
+	schedule: null,
+	error: null,
+};
+
+export function scheduleReducer(state = initialState, action) {
+	var newState = state;
+	var schedules;
+	switch (action.type) {
+		case SET_SCHEDULES:
+			newState = {...state, schedules: action.schedules};
+			break;
+		case SET_SCHEDULE:
+			newState = {...state, schedule: action.schedule};
+			break;
+		case REMOVE_SCHEDULE:
+			const lastRemovedSchedule = state.schedules.find((schedule) => schedule._id === action.scheduleId);
+			schedules = state.schedules.filter((schedule) => schedule._id !== action.scheduleId);
+			newState = {...state, schedules, lastRemovedSchedule};
+			break;
+		case ADD_SCHEDULE:
+			newState = {...state, schedules: [...state.schedules, action.schedule]};
+			break;
+		case UPDATE_SCHEDULE:
+			schedules = state.schedules.map((schedule) =>
+				schedule._id === action.schedule._id ? action.schedule : schedule
+			);
+			newState = {...state, schedules};
+			break;
+		case ADD_SCHEDULE_MSG:
+			newState = {...state, schedule: {...state.schedule, msgs: [...(state.schedule.msgs || []), action.msg]}};
+			break;
+		case SET_ERROR:
+			newState = {...state, error: action.error};
+			break;
+		default:
+	}
+	return newState;
+}
+
+// unitTestReducer()
